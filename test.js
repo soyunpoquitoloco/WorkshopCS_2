9dc413be78da7a11a8d1c4ef067d3c0c419da049
soyunpoquitoloco <160536173+soyunpoquitoloco@users.noreply.github.com>
2024-10-11 17:07:47 +0200

    make the final stage of the project

--- test.js
+++ test.js
@@ -2,6 +2,13 @@ const connectButton = document.getElementById('connect-button');
 const sendButton = document.getElementById('send-button');
 const messageInput = document.getElementById('message-input');
 const outputDiv = document.getElementById('output');
+const sendButtonf = document.getElementById('send-button-frequency');
+const frequencyInput = document.getElementById('frequency-input');
+const outputUltra = document.getElementById('outputUltrasonic');
+const outputHum = document.getElementById('outputHum');
+const outputTemp = document.getElementById('outputTemp');
+const outputLED = document.getElementById('outputLED');
+
 
 let socket = null;
 const domainName = '10.1.224.75'; // Remplacez par votre domaine ou votre service DDNS
@@ -9,26 +16,47 @@ const domainName = '10.1.224.75'; // Remplacez par votre domaine ou votre servic
 
   socket.onopen = () => {
     console.log('Connexion établie');
-    outputDiv.innerHTML += 'Connexion établie<br>';
+    outputDiv.innerHTML += 'Connection established<br>';
   };
 
   socket.onmessage = (event) => {
     console.log(`Message reçu : ${event.data}`);
-    outputDiv.innerHTML += `Message reçu : ${event.data}<br>`;
+    if(event.data[0]=="d"){
+      outputUltra.innerHTML = ` ${event.data.substr(1)}<br>`;
+    }
+    if(event.data[0]=="h"){
+      outputHum.innerHTML = ` ${event.data.substr(1)}<br>`;
+    }
+    if(event.data[0]=="t"){
+      outputTemp.innerHTML = ` ${event.data.substr(1)}<br>`;
+    }
+    if(event.data[0]=="f"){
+      outputLED.innerHTML = ` ${event.data.substr(1)}<br>`;
+    }
+    if(event.data=="Message well received !"){
+      outputDiv.innerHTML += `${event.data}<br>`;
+    }
+    //outputDiv.innerHTML += ` ${event.data}<br>`;
   };
 
   socket.onclose = () => {
     console.log('Connexion fermée');
-    outputDiv.innerHTML += 'Connexion fermée<br>';
+    outputDiv.innerHTML += 'Connection closed<br>';
   };
 
   socket.onerror = (error) => {
     console.log(`Erreur : ${error}`);
-    outputDiv.innerHTML += `Erreur : ${error}<br>`;
+    outputDiv.innerHTML += `Error : ${error}<br>`;
   };
 
 sendButton.addEventListener('click', () => {
   const message = messageInput.value;
   socket.send(message);
   messageInput.value = '';
+});
+
+sendButtonf.addEventListener('click', () => {
+  const message = "f" + frequencyInput.value;
+  socket.send(message);
+  frequencyInput.value = '';
 });
